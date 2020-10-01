d23726da91bcab70442df4a6b297638fd97f9064
MiyuruWijesinghe <mkwijesinghe2016@gmail.com>
2020-10-01 13:03:40 +0530

    added admin navbar to pages

--- app/src/main/java/com/example/studentinfo/AdminAdd.java
+++ app/src/main/java/com/example/studentinfo/AdminAdd.java
@@ -1,7 +1,7 @@
 package com.example.studentinfo;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -17,7 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class AdminAdd extends AppCompatActivity {
 
     EditText txtAdminId, txtFirstName, txtLastName;
-    ImageView submitBtn;
+    ImageView submitBtn, menuBtn;
     DatabaseReference dbRef;
     Admin admin;
 
